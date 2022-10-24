8d790917e706519338ed54b7b5a8cc2c4ceb7eb1
Justin Paul <justin@jpaul.me>
2022-10-23 22:21:36 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env pwsh
-Get-Module -ListAvailable | Import-Module
-Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -confirm:$false
