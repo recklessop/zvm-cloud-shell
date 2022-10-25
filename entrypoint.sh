d2b03963a10242ff924dd9f9d1fd97cc0c95fc52
Justin Paul <justin@jpaul.me>
2022-10-25 07:20:25 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+ 
+FILE="/zertoshell/autorun.ps1"
+ 
+if [ -f $FILE ];then
+  echo "Auto run file exists"
+  /gotty --once -w pwsh $FILE
+else
+  echo "Auto run file doesnt exist"
+  /gotty --once -w pwsh
+fi
 
-/gotty --once -w pwsh
